363bef9107d77fdfc6a6222ad81f673a7f414b44
unknown <rotba@WINCS.CS.BGU.AC.IL>
2018-11-05 15:54:03 +0200

    init

---
+++ docs/res/shaders/texShader.glsl
@@ -0,0 +1,20 @@
+#version 330 
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform float time;
+uniform vec4 coeffs;
+uniform vec4 src_lines[20];
+uniform vec4 dst_lines[20];
+uniform ivec4 sizes; 
+
+in vec2 uv;
+in vec3 position1;
+
+
+
+void main()
+{
+	gl_FragColor = time *texture(texture1,uv ) + (1 - time)*texture(texture2,uv);	
+}
+
