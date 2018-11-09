fbc20a7d2bd895acdc0a25609a96a64c32ca5cc4
youvalg <yuvalgeva77@gmail.com>
2018-11-09 18:03:33 +0200

    working on morph_pixel()

--- res/shaders/texShader.glsl
+++ res/shaders/texShader.glsl
@@ -38,16 +38,35 @@ vec4 interpolate_vecs(vec4 src_line, vec4 dst_line)
     ans[3] = time*src_line[3]+(1-time)dst_line[3];
     return ans;
 }
+
+
 #ans is a avg vector src_line and dst_line
-vec4 morph_pixel(vec4 src_line, vec4 average_vecs)
+vec4 morph_pixel(vec4 orig_lines[], vec4 average_vecs[])
 {
     int num_of_vecs = sizes[0];
 	vec2 uv_array[num_of_vecs];
+    vec2 dsum;
+    float weightsum = 0;
+    float a = coeffs[1];
+    float b = coeffs[2];
+    float p = coeffs[3];
     for(int i=0; i<num_of_vecs]; i++){
         vec2 uv_i;
         uv_i[0] =average_vecs[0]-average_vecs[2]
         uv_array[i] = uv_i;
+        vec2 displacment_i = vec2(uv.x-x_i_tag.x, uv.y-x_i_tag.y);
+        float dist_i = calc_dist_point_vec(uv, orig_lines[i]); 
+        float length_i = distance(orig_lines[i].xy,orig_lines[i].zw);
+        float weight_i = pow(pow(length_i,p)/(a+dist_i),b);
+        dsum = dsum + displacment_i*weight;
+        weightsum = weightsum + weight;
     }
-    
+    return uv+dsum/weightsum;
+}
+
+#Returns the distance between point to vec
+float morph_pixel(vec2 point, vec4 vec)
+{
+    float ans;
     return ans;
 }
