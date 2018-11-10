04636bf0098712c335423ac61691532e636340b6
youvalg <yuvalgeva77@gmail.com>
2018-11-10 13:27:46 +0200

    No compilation error. Todo: calc_dist_point_vec()

--- res/shaders/texShader.glsl
+++ res/shaders/texShader.glsl
@@ -10,6 +10,15 @@ uniform ivec4 sizes;
 in vec2 uv;
 in vec3 position1;
 
+#define MAX_NUM_OF_VECS 20
+
+vec4[MAX_NUM_OF_VECS] calc_average_vec();
+vec4 interpolate_vecs(vec4 src_line, vec4 dst_line);
+vec4 morph_pixel(vec4 orig_lines[MAX_NUM_OF_VECS], vec4 average_vecs[MAX_NUM_OF_VECS]);
+vec2 perpendicular(vec2 pq);
+
+
+
 void main()
 {
 	int num_of_vecs = 20;
@@ -18,28 +27,28 @@ void main()
 }
 
 //Calculate the set of the "average" vectors of the current output picture
-vec4[] calc_average_vec()
+vec4[MAX_NUM_OF_VECS] calc_average_vec()
 {
 	int num_of_vecs = sizes[0];
-    vec4 ans[num_of_vecs];
+    vec4 ans[MAX_NUM_OF_VECS];
 	for(int i=0; i<num_of_vecs; i++){
         ans[i] = interpolate_vecs(src_lines[i], dst_lines[i]);
     }
-    return ans;
+	return ans;
 }
 
 //ans is a avg vector src_line and dst_line
 vec4 interpolate_vecs(vec4 src_line, vec4 dst_line)
 {
 	vec4 ans;
-    ans[0] = time*src_line[0]+(1-time)dst_line[0];
-    ans[1] = time*src_line[1]+(1-time)dst_line[1];
-    ans[2] = time*src_line[2]+(1-time)dst_line[2];
-    ans[3] = time*src_line[3]+(1-time)dst_line[3];
+    ans[0] = time*src_line[0]+(1-time)*dst_line[0];
+    ans[1] = time*src_line[1]+(1-time)*dst_line[1];
+    ans[2] = time*src_line[2]+(1-time)*dst_line[2];
+    ans[3] = time*src_line[3]+(1-time)*dst_line[3];
     return ans;
 }
 //ans is a avg vector src_line and dst_line
-vec4 morph_pixel(vec4 orig_lines[], vec4 average_vecs[])
+vec4 morph_pixel(vec4 orig_lines[MAX_NUM_OF_VECS], vec4 average_vecs[MAX_NUM_OF_VECS])
 {
     int num_of_vecs = sizes[0];
 	vec2 dsum;
@@ -47,22 +56,22 @@ vec4 morph_pixel(vec4 orig_lines[], vec4 average_vecs[])
     float a = coeffs[1];
     float b = coeffs[2];
     float p = coeffs[3];
-    for(int i=0; i<num_of_vecs]; i++){
+    for(int i=0; i<num_of_vecs; i++){
         vec2 q_i=vec2(average_vecs[i][0],average_vecs[i][1]);
     	vec2 p_i=vec2(average_vecs[i][2],average_vecs[i][3]);
         vec2 p_i_tag= vec2(orig_lines[i][2],orig_lines[i][3]);
         vec2 q_i_tag= vec2(orig_lines[i][0],orig_lines[i][1]);
-        float u_i=((uv-p_i)*(q_i-p_i))/(pow((q_i[0]-p_i[0]),2)+pow((q_i[1]-p_i[1]),2));
-        float v_i=((uv-p_i)*perpendicular(q_i-p_i))/(sqrt(pow(q_i[0]-p_i[0],2) + pow((q_i[1]-p_i[1]),2)));
-        vec2 x_i_tag = p_i_tag + u_i*(q_i_tag-p_i_tag) + (v_i*peprpendicular(q_i_tag-p_i_tag))/(sqrt(pow(q_i[0]-p_i[0],2) + pow((q_i[1]-p_i[1]),2)));
+        float u_i=(dot((uv-p_i),(q_i-p_i)))/(pow((q_i[0]-p_i[0]),2)+pow((q_i[1]-p_i[1]),2));
+        float v_i=( dot( (uv-p_i) , perpendicular(q_i-p_i) ))/( sqrt( pow(q_i[0]-p_i[0],2) + pow((q_i[1]-p_i[1]),2) ) );
+        vec2 x_i_tag = p_i_tag + u_i*(q_i_tag-p_i_tag) + (v_i*perpendicular(q_i_tag-p_i_tag))/(sqrt(pow(q_i[0]-p_i[0],2) + pow((q_i[1]-p_i[1]),2)));
 		vec2 displacment_i = x_i_tag-uv;
         float dist_i = calc_dist_point_vec(uv, average_vecs[i]); 
 		//problem
         float length_i = distance(average_vecs[i].xy,average_vecs[i].zw);
 		//problem
         float weight_i = pow(pow(length_i,p)/(a+dist_i),b);
-        dsum = dsum + displacment_i*weight;
-        weightsum = weightsum + weight;
+        dsum = dsum + displacment_i*weight_i;
+        weightsum = weightsum + weight_i;
     }
     return uv+dsum/weightsum;
 }
